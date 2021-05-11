cc67f622ccb2fbecccb7a178760abec5b5543773
githubltx <1753232097@qq.com>
2021-05-11 10:08:12 +0800

    commit

--- zookeeper/src/main/java/com/jiuqi/zk/TsetDemo.java
+++ zookeeper/src/main/java/com/jiuqi/zk/TsetDemo.java
@@ -2,6 +2,6 @@ package com.jiuqi.zk;
 
 public class TsetDemo {
     public static void main(String[] args) {
-        System.out.println("hello ZK!!!");
+        System.out.println("hello ZooKeeper!!!");
     }
 }
