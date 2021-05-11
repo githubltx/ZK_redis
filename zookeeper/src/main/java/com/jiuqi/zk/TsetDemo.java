7de5c22532e463f1c0d3b4d0ec0f32a9c3018bae
githubltx <1753232097@qq.com>
2021-05-11 11:35:36 +0800

    dfd666

--- zookeeper/src/main/java/com/jiuqi/zk/TsetDemo.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.jiuqi.zk;
-
-public class TsetDemo {
-    public static void main(String[] args) {
-        System.out.println("hello ZooKeeper!!!");
-    }
-}
--- zookeeper/src/main/java/com/jiuqi/zk/ZK.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.jiuqi.zk;
-
-public class ZK {
-    public static void main(String[] args) {
-
-    }
-}
